63142f31b0c8a082df8272907374380f6eac60b5
prince5609 <prince.tyagi98@gmail.com>
2021-04-27 09:50:50 +0530

    practice problems 5

---
+++ diff n&21.py
@@ -0,0 +1,13 @@
+'''  QUESTION=
+Given an int n, return the absolute difference between n and 21
+except return double the absolute difference if n is over 21.
+'''
+
+def diff_21(n):
+    if n<=21:
+        diff=(21-n)
+        return diff
+    else:
+        diff=(n-21)*2
+        return diff
+print(diff_21(float(input("ENTER THE VALUE OF N :"))))

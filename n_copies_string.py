038315eac0fc19e68279148b888182b1488baf6f
prince5609 <prince.tyagi98@gmail.com>
2021-04-27 09:56:24 +0530

    practice problems 11

---
+++ n_copies_string.py
@@ -0,0 +1,24 @@
+''' QUESTION=
+
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+'''
+'''
+method 1--
+
+def n_copies(s,n):
+    copy=(s)*n
+    return copy
+print(n_copies(str(input("enter the string :")),int(input("enter value of n :"))))
+'''
+
+# SECOND METHOD
+def n_copy(s,n):
+    copy=s
+    for i in range(n-1):
+        copy=copy+s
+    return copy
+print(n_copy(str(input("enter the string :")),int(input("enter value of n :"))))
